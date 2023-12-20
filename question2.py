9bf8d053041bdab29be7332dc61fbddf2688d63f
Samuel-2552 <201501502@rajalakshmi.edu.in>
2023-12-20 17:21:29 +0530

    Question 2 completed

--- question2.py
+++ question2.py
@@ -1,12 +1,19 @@
 dice_a = 6
 dice_b = 6
-possibilites=0
-print('Dice |', *range(1,7), sep="\t")
-print("-----|","-"*44, sep="")
-
+print('Dice', '|', *range(1,7), sep="\t")
+print("--------|","-"*50, sep="")
+distribution={}
 for i in range(1,dice_a+1):
+    print(i ,"|", sep='\t', end="\t")
     for j in range(1,dice_b+1):
-        possibilites +=1
-        print(i, j, sep="\t")
-print("----"*4)
-print("Therefore the total no of combinations are: ", possibilites)
+        sum = i+j
+        distribution[sum] = distribution.get(sum,0)+1
+        print(sum, '', sep="\t", end="")
+    print()
+print("-"*60)
+print("The distribution is as follows: ")
+print("-"*97)
+print("|", *distribution.keys(), sep="\t", end="\t|\n")
+print("-"*97)
+print("|", *distribution.values(), sep="\t", end="\t|\n")
+print("-"*97)
